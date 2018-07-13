1cf4b118f68aa64e0819ebb6ad0f677da561b95d
abi-wilson25 <agibailw@gmail.com>
2018-07-13 12:55:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+let index = 0
 function init() {
   // your code here
-}
+  const keyCode = e.keyCode;
+  
+  if (keyCode === codes.length[index]){
+    index++;
+    if (index === codes.length){
+      alert ("Hurray!");
+      index= 0;
+    }
+  }
+  else {
+    index = 0
+  }
+  
+} 
